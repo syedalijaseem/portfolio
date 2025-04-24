fbc04de01aa4ed35004c4137e7bf7c4e49edb56e
syedalijaseem <syedali0057@gmail.com>
2025-04-23 19:29:05 -0500

    Resolved issues

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression({ algorithm: "gzip" }),
-    viteCompression({ algorithm: "brotliCompress" }),
-  ],
-  build: {
-    target: "es2020",
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 600,
-  },
+  plugins: [react()],
 });
